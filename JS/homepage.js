997412815168032a69524a031f8d5b09f2ea94ed
Gunnar Funderburk <alfredpysemail@gmail.com>
2024-04-09 16:34:15 -0400

    update

--- JS/homepage.js
+++ JS/homepage.js
@@ -10,21 +10,18 @@ function change_background() {
   console.log("Change Background");
 
   const containerElement = document.getElementById("image");
-  const textElement = document.querySelector(".text");
 
   const fadeOutEffect = setInterval(function () {
     if (!containerElement.style.opacity) {
       containerElement.style.opacity = 1;
-      textElement.style.opacity = 1; // Ensure text remains visible during transition
     }
     if (parseFloat(containerElement.style.opacity) > 0) {
       containerElement.style.opacity -= 0.1;
-      textElement.style.opacity -= 0.1; // Fade out the text along with the background image
     } else {
       clearInterval(fadeOutEffect);
       containerElement.style.backgroundImage =
         "url('" + backgrounds[curr_img_index] + "')";
-      fadeIn(textElement); // Pass the text element to fadeIn function
+      fadeIn(containerElement); // Call fadeIn function after background image is changed
     }
   }, 200);
 
@@ -34,9 +31,7 @@ function change_background() {
   }
 }
 
-function fadeIn(textElement) {
-  const containerElement = document.getElementById("image");
-
+function fadeIn(containerElement) {
   const fadeInEffect = setInterval(function () {
     if (!containerElement.style.opacity) {
       containerElement.style.opacity = 0;
@@ -44,11 +39,10 @@ function fadeIn(textElement) {
     if (parseFloat(containerElement.style.opacity) < 1) {
       containerElement.style.opacity =
         parseFloat(containerElement.style.opacity) + 0.1;
-      textElement.style.opacity = parseFloat(textElement.style.opacity) + 0.1; // Ensure text remains visible during transition
     } else {
       clearInterval(fadeInEffect);
     }
   }, 200);
 }
 
-setInterval(change_background, 5000);
+setInterval(change_background, 2000);
