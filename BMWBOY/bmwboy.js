73f491774a8c3bbf9d521e01265aae04ab9c391d
Gunnar Funderburk <glfunderburk@protonmail.com>
2026-06-15 23:21:02 -0400

    Sounds hopefully

--- BMWBOY/bmwboy.js
+++ BMWBOY/bmwboy.js
@@ -1,7 +1,9 @@
-var current_game=null;
-canvas_objects=[];
-canvas_height=0;
+var current_game = null;
+canvas_objects = [];
+canvas_height = 0;
 let settledCanvas, settledCtx;
+let canvas, ctx;
+var power = false;
 
 function calculate_coord(equationX, equationY, index) {
   const x = math.evaluate(equationX, { t: index });
@@ -9,21 +11,21 @@ function calculate_coord(equationX, equationY, index) {
   return { x, y };
 }
 
-class DrawingObject{
-  constructor(x, y, color){
+class DrawingObject {
+  constructor(x, y, color) {
     this.x = x;
     this.y = y;
     this.color = color;
   }
 }
 
-class StringObject extends DrawingObject{
-  constructor(x, y, color, font, text){
+class StringObject extends DrawingObject {
+  constructor(x, y, color, font, text) {
     super(x, y, color);
     this.text = text;
     this.font = font;
   }
-  draw(ctx){
+  draw(ctx) {
     ctx.font = this.font;
     ctx.fillStyle = this.color;
     ctx.fillText(this.text, this.x, this.y);
@@ -32,7 +34,7 @@ class StringObject extends DrawingObject{
 
 function animation_draw(ctx, object, startx, starty, equationX, equationY, step_time, steps, velocity = 1, onComplete) {
   let i = 0;
-  
+
   function animate() {
     if (i <= steps) {
       const coord = calculate_coord(equationX, equationY, i);
@@ -56,26 +58,36 @@ function animation_draw(ctx, object, startx, starty, equationX, equationY, step_
 }
 
 btn_ids = ["btn-a", "btn-b", "btn-start", "btn-select", "btn-up", "btn-down", "btn-left", "btn-right"];
-for(let i = 0; i < btn_ids.length; i++){
-    console.log(btn_ids[i], document.getElementById(btn_ids[i]));
+for (let i = 0; i < btn_ids.length; i++) {
+  console.log(btn_ids[i], document.getElementById(btn_ids[i]));
 }
-for(let i = 0; i < btn_ids.length; i++){
-    let btn = document.getElementById(btn_ids[i]);
-    btn.addEventListener("touchstart", function(e){
-        console.log("pressed " + btn_ids[i]);
-        if(current_game != null){
-             e.preventDefault();
-            current_game.controller.press(btn_ids[i]);
-        }
-    });
+for (let i = 0; i < btn_ids.length; i++) {
+  let btn = document.getElementById(btn_ids[i]);
+  btn.addEventListener("touchstart", function (e) {
+    console.log("pressed " + btn_ids[i]);
+    if (current_game != null) {
+      e.preventDefault();
+      current_game.controller.press(btn_ids[i]);
+    }
+  });
 }
+
+var start_button = document.getElementById('btn-start');
+start_button.addEventListener("touchstart", function (e) {
+  e.preventDefault();
+  if (!power) {
+    power = true; // prevent double firing
+     const sound = new Audio('boot.mp3');
+    sound.play();
+    logo_draw();
+  }
+});
 
 function scaleButtons() {
   const consoleImg = document.getElementById('console-img');
-  const consoleContainer = document.getElementById('console');
-  
+
   const scale = consoleImg.width / 340;
-  
+
   const buttons = [
     { id: 'btn-a', x: 239, y: 307, w: 50, h: 50 },
     { id: 'btn-b', x: 185, y: 340, w: 50, h: 50 },
@@ -86,7 +98,7 @@ function scaleButtons() {
     { id: 'btn-left', x: 20, y: 348, w: 20, h: 20 },
     { id: 'btn-right', x: 59, y: 348, w: 20, h: 20 }
   ];
-  
+
   buttons.forEach(btn => {
     const el = document.getElementById(btn.id);
     el.style.left = (btn.x * scale) + 'px';
@@ -96,40 +108,37 @@ function scaleButtons() {
   });
 }
 
-function logo_draw(){
-  const canvas = document.getElementById('screen');
-  const ctx = canvas.getContext('2d');
-  
-  const letter = new StringObject(0, canvas_height - 100, '#ccc8c0', '120px Arial', 'BMWBOY\u00AE');
-  const letter2 = new StringObject(0, canvas_height - 100, '#08241c', '110px Arial', 'Matt Edition');
-  
-  animation_draw(ctx, letter, 150, canvas_height-700, "0", "-t", 100, 400, 10, () => {
-    animation_draw(ctx, letter2, 150, canvas_height-600, "0", "-t", 100, 400, 10);
-  });
-}
-
-function drawtoScreen(){
-  const canvas = document.getElementById('screen');
+function initScreen() {
+  canvas = document.getElementById('screen');
   const screenArea = document.getElementById('screen-area');
 
-  canvas.width = screenArea.offsetWidth
+  canvas.width = screenArea.offsetWidth;
   canvas.height = screenArea.offsetHeight;
   canvas_height = canvas.height;
-  const ctx = canvas.getContext('2d');
- 
+  ctx = canvas.getContext('2d');
+
   settledCanvas = document.createElement('canvas');
   settledCanvas.width = canvas.width;
   settledCanvas.height = canvas.height;
   settledCtx = settledCanvas.getContext('2d');
+}
+
+function logo_draw() {
+  initScreen();
 
   const img = new Image();
   img.src = 'bmwboyback.png';
   img.onload = () => {
     settledCtx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    logo_draw();
+
+    const letter = new StringObject(0, canvas_height - 100, '#ccc8c0', '120px Arial', 'BMWBOY\u00AE');
+    const letter2 = new StringObject(0, canvas_height - 100, '#08241c', '110px Arial', 'Matt Edition');
+
+    animation_draw(ctx, letter, 150, canvas_height - 700, "0", "-t", 100, 400, 5, () => {
+      animation_draw(ctx, letter2, 150, canvas_height - 600, "0", "-t", 100, 400, 5);
+    });
   };
 }
 
-drawtoScreen();
 window.addEventListener('resize', scaleButtons);
 scaleButtons();
